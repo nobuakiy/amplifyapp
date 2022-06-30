52d928af3b7d9c653eaa3a210e41389ee0f9cbb5
Nobuaki Yamashita <nobuakiy09@gmail.com>
2022-06-30 17:48:34 +0900

    small change to view updates works

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, you've done.
+          Hello, you've done!
         </p>
         <a
           className="App-link"
